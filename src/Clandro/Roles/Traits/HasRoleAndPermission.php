757a1cd092f9e600bd6ef0d4a05ff05e6658926d
Alejandro Hernandez <a.hernandez@traxion.global>
2019-12-01 10:56:51 -0600

    Fix snake case function

--- src/Clandro/Roles/Traits/HasRoleAndPermission.php
+++ src/Clandro/Roles/Traits/HasRoleAndPermission.php
@@ -327,7 +327,7 @@ trait HasRoleAndPermission
     public function detachAllPermissions()
     {
         $this->permissions = null;
-        
+
         return $this->userPermissions()->detach();
     }
 
@@ -384,12 +384,12 @@ trait HasRoleAndPermission
      */
     public function __call($method, $parameters)
     {
-        if (starts_with($method, 'is')) {
-            return $this->is(snake_case(substr($method, 2), config('roles.separator')));
-        } elseif (starts_with($method, 'can')) {
-            return $this->can(snake_case(substr($method, 3), config('roles.separator')));
-        } elseif (starts_with($method, 'allowed')) {
-            return $this->allowed(snake_case(substr($method, 7), config('roles.separator')), $parameters[0], (isset($parameters[1])) ? $parameters[1] : true, (isset($parameters[2])) ? $parameters[2] : 'user_id');
+        if (Str::startsWith($method, 'is')) {
+            return $this->is(Str::snake(substr($method, 2), config('roles.separator')));
+        } elseif (Str::startsWith($method, 'can')) {
+            return $this->can(Str::snake(substr($method, 3), config('roles.separator')));
+        } elseif (Str::startsWith($method, 'allowed')) {
+            return $this->allowed(Str::snake(substr($method, 7), config('roles.separator')), $parameters[0], (isset($parameters[1])) ? $parameters[1] : true, (isset($parameters[2])) ? $parameters[2] : 'user_id');
         }
 
         return parent::__call($method, $parameters);
